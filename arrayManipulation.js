93c2236e7cdb8a4f094c312cbc0695bccb066080
unknown <emensah670@st.ug.edu.gh>
2024-05-18 23:32:18 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,3 +8,24 @@ function processArray (array){
  const array = [2,3,5,7,8]
  const result = processArray(array)
  console.log(result)
+
+ function formatArrayStrings(strArr, result) {
+    // Iterate over the arrays and modify each string based on its corresponding number
+    for (let i = 0; i < strArr.length; i++) {
+      // If the number is even, capitalize the entire string
+      if (result[i] % 2 === 0) {
+        strArr[i] = strArr[i].toUpperCase();
+      }
+      // If the number is odd, convert the string to lowercase
+      else {
+        strArr[i] = strArr[i].toLowerCase();
+      }
+    }
+  
+    // Return the modified string array
+    return strArr;
+  }
+
+  const strArr = ['kofi','Ama','Yaw','Abena','Kwame']
+  const modifiedNames = formatArrayStrings(strArr, result)
+  console.log(modifiedNames)
